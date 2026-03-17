1932528323ec8cf5b882f63a6ce9743a83aaf976
Francisco Rodrigues <francisco.ribeiro.rodriguess00@gmail.com>
2026-03-17 17:35:31 +0000

    vercel Web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { QueryProvider } from "@/components/providers/QueryProvider";
 import { DarkModeHandler } from "@/components/layout/DarkModeHandler";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -27,6 +28,7 @@ export default function RootLayout({
           <DarkModeHandler />
           {children}
           <Toaster richColors position="top-center" />
+          <Analytics />
           <SpeedInsights />
         </QueryProvider>
       </body>
